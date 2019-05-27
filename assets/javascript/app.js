e6a9877204b2e0b1fdde323b63d1004aee246e52
TerrenceMM2 <terrencemm2@gmail.com>
2019-05-26 22:33:04 -0500

    adds fav icons; working on toggle to show new icon when selected

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,6 +11,11 @@ $(document).ready(function () {
     var activeGif = "";
     var offsetNumber = 0;
 
+    // var nutritionTopic = activeGif;
+    // var nutritionApiKey = "ef1557de06mshdcf3099eaace12dp128ffejsn390f022c636c";
+    // var nutritionURL = "https://nutritionix-api.p.rapidapi.com/v1_1/search/";
+    // var nutritionString = "";
+
     for (var i = 0; i < topic.length; i++) {
         generateButton(topic[i]);
     };
@@ -18,9 +23,9 @@ $(document).ready(function () {
     $("#button-group").on("click", ".btn", function () {
         $("#more-gifs").show();
         offsetNumber = 0;
-        activeGif = $(this).text();
         clearGifs();
         topicGifs = [];
+        activeGif = $(this).text();
         queryTopic = $(this).text();
         generateURL(queryTopic, offsetNumber);
         getGifs(queryString);
@@ -43,12 +48,17 @@ $(document).ready(function () {
         var searchedWord = $("#user-input").val().trim();
         topicGifs.push(searchedWord);
         generateButton(searchedWord);
+        $("#user-input").val("");
     });
 
     $("#more-gifs").on("click", function () {
         event.preventDefault();
         generateURL(activeGif, offsetNumber);
         getGifs(queryString);
+    });
+
+    $("#fav").on("click", function () {
+        console.log(this);
     });
 
     function generateButton(str) {
@@ -60,6 +70,10 @@ $(document).ready(function () {
     function generateURL(str, num) {
         return queryString = queryURL + "api_key=" + queryApiKey + "&" + "q=" + str + "&" + "limit=10" + "&" + "offset=" + num;
     };
+
+    // function generateNutritionURL(str) {
+    //     return nutritionString = nutritionURL + nutritionTopic + "&" + "api_key=" + nutritionApiKey;
+    // }
 
     function getGifs(str) {
         $.ajax({
@@ -81,14 +95,23 @@ $(document).ready(function () {
             var imageGif = $("<img>").attr("src", topicGifs[k].images.fixed_height_still.url);
             imageGif.addClass("gif figure-img img-fluid rounded");
             imageGif.attr("alt", topicGifs[k].title);
-            // imageGif.attr("data-value", num);
+            imageGif.attr("data-value", k);
             imageGif.attr("data-moving", "off");
+            var imageFav = $("<figcaption>");
+            var imageText = '<i id="fav" class="fa-2x far fa-star float-right"></i>';
+            imageFav.html(imageText);
             var imageRating = $("<figcaption>");
             var ratingText = "Rated: " + topicGifs[k].rating.toUpperCase();
             imageRating.text(ratingText);
             imageRating.addClass("figure-caption text-left");
+            var imageTitle = $("<figcaption>");
+            var titleText = topicGifs[k].title.italics();
+            imageTitle.html(titleText);
+            imageTitle.addClass("figure-caption text-left");
             $(imageContainer).append(imageGif);
+            $(imageContainer).append(imageFav);
             $(imageContainer).append(imageRating);
+            $(imageContainer).append(imageTitle);
             $("#gif-gallery").append(imageContainer);
         };
     };
